97b336d3dee0279628169c2db9b05ad0171dd3fd
Eshan <2016mehrab@gmail.com>
2024-11-23 03:12:19 +0600

    feat: Add new JS snippets
    Add snippets for React functional components and basic functions.

--- lua/custom/snippets/custom-js-snippets.lua
+++ lua/custom/snippets/custom-js-snippets.lua
@@ -11,17 +11,18 @@ local insertNode = ls.insert_node
 local textNode = ls.text_node
 local snippetNode = ls.snippet_node
 
-ls.add_snippets('javascript', {
-  s('cl', {
-    textNode 'console.log(',
-    insertNode(1),
-    textNode ');',
-  }),
-})
+-- ls.add_snippets('javascript', {
+--   s('cl', {
+--     textNode 'console.log(',
+--     insertNode(1),
+--     textNode ');',
+--   }),
+-- })
 
+-- NOTE: REACT FUNCTIONAL COMPONENT NAMED EXPORT
 ls.add_snippets('javascript', {
   s(
-    'fce',
+    'rafce',
     fmt(
       [[
       export const {} = () => {{
@@ -36,6 +37,23 @@ ls.add_snippets('javascript', {
         insertNode(1),
         -- NOTE: repeat contents of node 1
         repetition(1),
+      }
+    )
+  ),
+})
+
+-- NOTE: BASIC FUNCTION
+ls.add_snippets('javascript', {
+  s(
+    'func',
+    fmt(
+      [[
+      export function {}() {{
+        return {{}};
+      }};
+    ]],
+      {
+        insertNode(1),
       }
     )
   ),
