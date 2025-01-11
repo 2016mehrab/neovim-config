8ea6ea1ebcffafaa263dc93781ff19cc25742a71
Eshan <2016mehrab@gmail.com>
2025-01-11 22:28:24 +0600

    chore: update mason.nvim config option
    - Changed config to opts in lazy.setup

--- init.lua
+++ init.lua
@@ -445,7 +445,7 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+      { 'williamboman/mason.nvim', opts = {} }, -- NOTE: Must be loaded before dependants
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
       'nvim-java/nvim-java',
